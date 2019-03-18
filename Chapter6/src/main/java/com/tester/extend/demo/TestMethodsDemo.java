9f13d9c61bb0307434692d31072dedf724ed1f9a
JiapengHou <937396342@qq.com>
2019-03-18 18:02:28 +0800

    moco get post

---
+++ Chapter6/src/main/java/com/tester/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,27 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+        System.out.println();
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+    @Test
+    public void logDemo(){
+        Reporter.log("This is my first log...");
+        throw  new RuntimeException("This is my runtime exception...");
+    }
+}
