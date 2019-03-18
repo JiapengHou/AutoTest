18101490a2898de72b8f984d7d700986a04e8998
JiapengHou <937396342@qq.com>
2019-03-18 13:13:19 +0800

    添加moco

---
+++ Chapter5/src/main/java/com/course/testng/multThread/MUltThreadOnAnnotion.java
@@ -0,0 +1,12 @@
+package com.course.testng.multThread;
+
+import org.testng.annotations.Test;
+
+public class MUltThreadOnAnnotion {
+    @Test(invocationCount = 10,threadPoolSize = 3)
+    public void test(){
+        System.out.println(1);
+        System.out.printf("Thread id: %s%n",Thread.currentThread().getId());
+    }
+
+}
